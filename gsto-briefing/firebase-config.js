eff4a79fe7edd9da8c1d013cdfe63540cfc574da
nbloong <96856219+nbloong@users.noreply.github.com>
2026-08-13 08:09:55 +0800

    Add GSTO Firebase configuration

---
+++ gsto-briefing/firebase-config.js
@@ -0,0 +1,8 @@
+window.GSTO_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAgG-gh5Vop6VjSmzfoyPUDZbpW1plrdkY",
+  authDomain: "training-system-31ee4.firebaseapp.com",
+  projectId: "training-system-31ee4",
+  storageBucket: "training-system-31ee4.firebasestorage.app",
+  messagingSenderId: "976628653434",
+  appId: "1:976628653434:web:e3d1bd753c968f4e048ac3"
+};
